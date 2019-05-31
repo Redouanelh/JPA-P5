d06071d0d4fb0f0e1ce96badd38ecce9c2193ef3
Redouanelh <redouan_school@outlook.com>
2019-05-31 13:13:31 +0200

    Initial commit

---
+++ src/hu/nl/hibernate/Main.java
@@ -0,0 +1,224 @@
+package hu.nl.hibernate;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import ovHibernate.OVchipkaart;
+import reizigerHibernate.Reiziger;
+
+
+public class Main {
+  private static SessionFactory factory;
+  public static void main(String[] args) throws SQLException, ParseException {
+      try {
+        factory = new Configuration().configure().buildSessionFactory();
+      } catch (Throwable ex) {
+        System.err.println("Failed to create sessionFactory object." + ex);
+        throw new ExceptionInInitializerError(ex);
+      }
+      
+      Reiziger updatedReiziger = new Reiziger(10, "HH", "Hibernate", "Update", Date.valueOf("2002-09-20"));
+      OVchipkaart updatedOv = new OVchipkaart(83928, 3.10, 2, Date.valueOf("2021-09-21"), 3);
+      
+      Main m = new Main();
+      
+//      m.addReiziger(9, "H", "de", "Hibernate", Date.valueOf("2002-09-17"));
+//      m.updateReiziger(9, updatedReiziger);
+//      m.deleteReiziger(9);
+//      m.listReizigers();
+            
+//      m.addOv(83928, 2.80, 1, Date.valueOf("2020-09-21"), 3);
+//      m.updateOv(83928, updatedOv);
+//      m.deleteOv(83928);
+      
+      // Methode maken find ov by id.
+      
+//      m.listOv();
+      
+      
+      System.out.println("success!");    
+  }
+  
+  public Integer addOv(int id, double sal, int klasse, Date geldigtot, int reizigerid) {
+	  Session session = factory.openSession();
+	  Transaction t = null;
+	  Integer ovid = null;
+	  
+	  try {
+		  t = session.beginTransaction();
+		  OVchipkaart ovkaart = new OVchipkaart(id, sal, klasse, geldigtot, reizigerid);
+		  ovid = (Integer) session.save(ovkaart);
+		  t.commit();
+	  } catch (HibernateException e) {
+		  if (t != null) t.rollback();
+		  e.printStackTrace();
+	  } finally {
+		  session.close();
+	  }
+	  return ovid;
+  }
+  
+  public Integer addReiziger(int id, String vl, String tvoegsel, String anaam, Date gbdatum) {
+	  Session session = factory.openSession();
+	  Transaction t = null;
+	  Integer reizigerid = null;
+	  
+	  try {
+		  t = session.beginTransaction();
+		  Reiziger reiziger = new Reiziger(id, vl, tvoegsel, anaam, gbdatum);
+		  reizigerid = (Integer) session.save(reiziger);
+		  t.commit();
+	  } catch (HibernateException e) {
+		  if (t != null) t.rollback();
+		  e.printStackTrace();
+	  } finally {
+		  session.close();
+	  }
+	  return reizigerid;
+  }
+  
+  public void listOv() {
+	  Session session = factory.openSession();
+	  Transaction t = null;
+	  
+	  try {
+		  t = session.beginTransaction();
+		  List ovkaarten = session.createQuery("FROM OVchipkaart").list();
+		  for (Iterator iterator = ovkaarten.iterator(); iterator.hasNext();) {
+			  OVchipkaart ovkaart = (OVchipkaart) iterator.next();
+			  System.out.println("Kaartnummer: " + ovkaart.getKaartnummer());
+			  System.out.println("Geldig tot: " + ovkaart.getGeldig());
+			  System.out.println("Klasse: " + ovkaart.getKlasse());
+			  System.out.println("Saldo: " + ovkaart.getSaldo());
+			  System.out.println("Reizigerid: " + ovkaart.getReizigerid() + "\n");
+
+		  }
+		  t.commit();
+	  } catch (HibernateException e) {
+		  if(t != null) t.rollback();
+		  e.printStackTrace();
+	  } finally {
+		  session.close();
+	  }  
+  }
+  
+  public void listReizigers() {
+	  Session session = factory.openSession();
+	  Transaction t = null;
+	  
+	  try {
+		  t = session.beginTransaction();
+		  List reizigers = session.createQuery("FROM Reiziger").list();
+		  for (Iterator iterator = reizigers.iterator(); iterator.hasNext();) {
+			  Reiziger reiziger = (Reiziger) iterator.next();
+			  System.out.println("Voorletters: " + reiziger.getVoorletter());
+			  System.out.println("Tussenvoegsel: " + reiziger.getTussenvoegsel());
+			  System.out.println("Achternaam: " + reiziger.getAchternaam());
+			  System.out.println("Geboortedatum: " + reiziger.getGbdatum() + "\n");
+			  // Hier moet ook nog de ov kaarten functie worden aangeroepen voor deze reiziger dus iets van find ov by reizigerid
+
+		  }
+		  t.commit();
+	  } catch (HibernateException e) {
+		  if(t != null) t.rollback();
+		  e.printStackTrace();
+	  } finally {
+		  session.close();
+	  }  
+  }
+  
+  public void updateOv(int id, OVchipkaart updatedOv) {
+	  Session session = factory.openSession();
+	  Transaction t = null;
+	  
+	  try {
+		  t = session.beginTransaction();
+		  OVchipkaart ovkaart = (OVchipkaart) session.get(OVchipkaart.class, id);
+		  ovkaart.setSaldo(updatedOv.getSaldo());
+		  ovkaart.setGeldig(updatedOv.getGeldig());
+		  ovkaart.setKlasse(updatedOv.getKlasse());
+		  ovkaart.setReizigerid(updatedOv.getReizigerid());
+		  
+		  session.update(ovkaart);
+		  t.commit();
+	  } catch (HibernateException e) {
+		  if (t != null) t.rollback();
+		  e.printStackTrace();
+	  } finally {
+		  session.close();
+	  }
+  }
+  
+  public void updateReiziger(int id, Reiziger updatedReiziger) {
+	  Session session = factory.openSession();
+	  Transaction t = null;
+	  
+	  try {
+		  t = session.beginTransaction();
+		  Reiziger reiziger = (Reiziger) session.get(Reiziger.class, id);
+		  reiziger.setVoorletter(updatedReiziger.getVoorletter());
+		  reiziger.setTussenvoegsel(updatedReiziger.getTussenvoegsel());
+		  reiziger.setAchternaam(updatedReiziger.getAchternaam());
+		  reiziger.setGbdatum(updatedReiziger.getGbdatum());
+		  
+		  session.update(reiziger);
+		  t.commit();
+	  } catch (HibernateException e) {
+		  if (t != null) t.rollback();
+		  e.printStackTrace();
+	  } finally {
+		  session.close();
+	  }
+  }
+  
+  public void deleteOv(int id) {
+	  Session session = factory.openSession();
+	  Transaction t = null;
+	  
+	  try {
+		  t = session.beginTransaction();
+		  OVchipkaart ovkaart = (OVchipkaart) session.get(OVchipkaart.class, id);
+		  
+		  session.delete(ovkaart);
+		  t.commit();	  
+	  } catch (HibernateException e) {
+		  if (t != null) t.rollback();
+		  e.printStackTrace();
+	  } finally {
+		  session.close();
+	  }
+	    
+  }
+  
+  public void deleteReiziger(int id) {
+	  Session session = factory.openSession();
+	  Transaction t = null;
+	  
+	  try {
+		  t = session.beginTransaction();
+		  Reiziger reiziger = (Reiziger) session.get(Reiziger.class, id);
+		  
+		  session.delete(reiziger);
+		  t.commit();	  
+	  } catch (HibernateException e) {
+		  if (t != null) t.rollback();
+		  e.printStackTrace();
+	  } finally {
+		  session.close();
+	  }
+	    
+  }
+  
+  
+  
+  
+}
---
+++ src/reizigerHibernate/Reiziger.java
@@ -0,0 +1,100 @@
+package reizigerHibernate;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import ovHibernate.OVchipkaart;
+
+public class Reiziger {
+	private int reizigerid;
+	private String voorletter;
+	private String tussenvoegsel;
+	private String achternaam;
+	private Date gbdatum;
+	private ArrayList<OVchipkaart> ovchipkaarten = new ArrayList<OVchipkaart>();
+	
+	public Reiziger() {
+	}
+	
+	
+	public Reiziger(int reizigerid, String voorletter, String tussenvoegsel, String achternaam, Date gbdatum) {
+		super();
+		this.reizigerid = reizigerid;
+		this.voorletter = voorletter;
+		this.tussenvoegsel = tussenvoegsel;
+		this.achternaam = achternaam;
+		this.gbdatum = gbdatum;
+	}
+
+	public int getReizigerid() {
+		return reizigerid;
+	}
+
+
+	public void setReizigerid(int reizigerid) {
+		this.reizigerid = reizigerid;
+	}
+
+
+	public Date getGbdatum() {
+		return gbdatum;
+	}
+
+
+	public void setGbdatum(Date gbdatum) {
+		this.gbdatum = gbdatum;
+	}
+
+
+	public ArrayList<OVchipkaart> getOvchipkaarten() {
+		return ovchipkaarten;
+	}
+
+
+	public void setOvchipkaarten(ArrayList<OVchipkaart> ovchipkaarten) {
+		this.ovchipkaarten = ovchipkaarten;
+	}
+
+
+	public void setTussenvoegsel(String tussenvoegsel) {
+		this.tussenvoegsel = tussenvoegsel;
+	}
+	
+	public void setVoorletter(String vl) {
+		voorletter = vl;
+	}
+	
+	public void setTussenVoegsel(String tv) {
+		tussenvoegsel = tv;
+	}
+	
+	public void setAchternaam(String an) {
+		achternaam = an;
+	}
+	
+	public String getTussenvoegsel() {
+		return tussenvoegsel;
+	}
+	
+	public String getAchternaam() {
+		return achternaam;
+	}
+	
+	public String getVoorletter() {
+		return voorletter;
+	}
+	
+	public void saveOv(OVchipkaart ov) {
+		ovchipkaarten.add(ov);
+	}
+	
+	public ArrayList<OVchipkaart> getOv() {
+		return ovchipkaarten;
+	}
+	
+	public void setOvKaarten(ArrayList<OVchipkaart> list) {
+		ovchipkaarten = list;
+	}
+	
+	
+}
